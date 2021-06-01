d59ea2aa198e2584a71b95db5d9a5a72a897d8eb
manoj <manoj@LAPTOP-JBIN0KND>
2021-06-01 15:00:06 +0530

    first commit

---
+++ myproject/src/myproject/Main.java
@@ -0,0 +1,19 @@
+package myproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+				int x=10;
+				float y=20.54f;
+				char c='x';
+				String s="I am the best";
+				
+				System.out.println(x);
+				System.out.println(y);
+				System.out.println(c);
+				System.out.println(s);
+			}
+
+	}
+
